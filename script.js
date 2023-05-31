00c353027d2bbe5bc40bd9309b098e1434bf6143
Antonio Zizzari <72402942+Antonio-Zizzari@users.noreply.github.com>
2023-05-31 21:24:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ var confetti = new ConfettiGenerator(confettiSettings);
 confetti.render();
 
  let p = document.createElement("p");
- p.innerText = "Congratulazione hai fatto la scelta giusta 🎉 \nAdesso ti tocca sposarmi scemina 😉";
+ p.innerText = "Congratulazioni hai fatto la scelta giusta 🎉 \nAdesso ti tocca sposarmi scemina ❤";
   p.style.fontSize = "2rem";
   p.style.fontWeight = "bold";
   p.style.textAlign = "center";
